ca72c66cf09576d1124052e26aeb45da4f49de49
David Enoma <davidenoma@gmail.com>
2021-11-19 17:44:00 +0100

    Addition of code

---
+++ neural-network-feature-importance.py
@@ -0,0 +1,60 @@
+# Imports
+import numpy as np
+import sklearn
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
+# Load the data
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Create a train/test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# Preprocess the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Create a classifier
+clf = MLPClassifier(hidden_layer_sizes=(8, 4),learning_rate_init=0.01)
+# Fit the classifier using the training set
+clf.fit(X_train, y_train)
+# Evaluate the classifier using the test set
+y_pred=clf.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+print(acc)
+
+def get_feature_importance(j, n):
+  s = accuracy_score(y_test, y_pred) # baseline score
+  total = 0.0
+  for i in range(n):
+    perm = np.random.permutation(range(X_test.shape[0]))
+    X_test_ = X_test.copy()
+    X_test_[:, j] = X_test[perm, j]
+    y_pred_ = clf.predict(X_test_)
+    s_ij = accuracy_score(y_test, y_pred_)
+    total += s_ij
+  return s - total / n
+
+# Feature importances
+f = []
+for j in range(X_test.shape[1]):
+  f_j = get_feature_importance(j, 100)
+  f.append(f_j)
+# Plot
+print(f)
+plt.figure(figsize=(10, 5))
+plt.bar(range(X_test.shape[1]), f, color="r", alpha=0.7)
+plt.xticks(ticks=range(X_test.shape[1]))
+plt.xlabel("Feature")
+plt.ylabel("Importance")
+plt.title("Feature importances (Iris data set)")
+plt.show()
+
+
+
+
---
+++ xgboost/optimization2.py
@@ -0,0 +1,54 @@
+#First we oprimize n_estimators = [50, 100], max_depth and learning_rate.
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import GridSearchCV
+
+# fixing seed: important to have same random train and test split as the optimizing
+np.random.seed(0)
+
+# loading data
+#  creating random genotyped data with the same size as the data used in the original manuscript
+# Note heterozygous and homozygous minor a
+# re encoded as 0, 1, and 2, respectively.
+#696, 125041
+#X = np.random.randint(3, size=(200, 1000))
+#Y = np.random.randint(2, size=(200, ))
+X = pd.read_csv('hapmap_geno_recoded',sep=" ")
+Y = pd.read_csv('hapmap_phenotype_recoded',sep=" ")
+print(X.shape)
+print(Y.shape)
+
+# Tuning
+NUM_TRIALS = 10
+n_estimators = [250, 300]
+max_depth = [2, 4, 6, 8]
+learning_rate = [0.001, 0.01, 0.1]
+# For the parameter tuning.
+param_grid = dict(max_depth=max_depth, n_estimators=n_estimators, learning_rate=learning_rate)
+model = XGBClassifier(seed=0, nthread=5)
+tot_grid_results = list()
+best_grid_results = list()
+for i in range(NUM_TRIALS):
+    print('testing trial',i)
+    # Dividing into training and test set.x
+    x, x_cv, y, y_cv = train_test_split(X, Y, test_size=0.2, train_size=0.8, stratify=Y,
+                                        random_state=i)
+    # optimizing xgboost parameters: never seen on x_cv and y_cv
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=i)
+    grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=5, cv=cv, verbose=1)
+    grid_result = grid_search.fit(x, y.values.ravel())
+    tot_grid_results.append(grid_result)
+    best_grid_results.append([grid_result.best_score_, grid_result.best_params_])
+
+# save the hyperparamters
+f = open('tot_grid_results_stage1_kuopio_2.pckl', 'wb')
+pickle.dump(tot_grid_results, f)
+f.close()
+
+f = open('best_grid_results_stage1_kuopio_2.pckl', 'wb')
+pickle.dump(best_grid_results, f)
+f.close()
