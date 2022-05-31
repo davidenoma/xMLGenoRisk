b9bb6bd17c7e5d75ccfc785ad21b0485316f5772
David Enoma <davidenoma@gmail.com>
2022-05-31 15:50:48 +0100

    Framework-for-the-different-ML-algorithms

--- ensembling_algorithm.py
+++ ensembling_algorithm.py
@@ -1,14 +1,35 @@
-import deeplearning
-import randomforest
-import xgboost_fp
+import pandas as pd
+from numpy import array
 
-#import scores
+
+#import scores from the different algorithms
 XGBOOST_TOP_SNPS = open("xgboost_fp/top_snps.list").read()
+XGBOOST_TOP_SNPS = open("xgboost_fp/top_snps.list").read()
+
 
 #find the set of overlap
 
 
 #locate this overlap to snps
 
+
+
 #use this snp set for risk prediction on the validation set
 
+
+
+
+#converting the indices to snps
+def prepare_header_snps(snps_file_name):
+    snps_file_name = 'snps_list_shorter.txt'
+    header_file = pd.read_csv(snps_file_name, header=None)
+    final_snps = []
+    optimal_indices_2 = array([6, 14, 109, 160, 236, 285, 299, 300, 307, 318, 323, 339, 413,
+                               429, 445, 450, 464, 475, 484, 513, 544, 564, 581, 584, 604, 613,
+                               614, 646, 680, 687, 704, 724, 735, 743, 772, 784, 798, 826, 849,
+                               856, 867, 874, 886, 893, 922, 940, 943, 983, 996, 997])
+    header_file_subset = header_file.iloc[:, optimal_indices_2]
+
+    for i in range(header_file_subset.shape[1]):
+        final_snps.append(str(header_file_subset.iloc[0, i]).split('_')[0])
+    header_file_string = str(header_file_subset)
