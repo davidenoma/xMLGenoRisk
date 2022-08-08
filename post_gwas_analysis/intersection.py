e944f2c4d53346b3686d5cac9d2ac2a9462fffb8
David Enoma <davidenoma@gmail.com>
2022-08-08 15:34:53 +0100

    postgwas analysis

---
+++ post_gwas_analysis/intersection.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+final_snps_on_file = pd.read_csv('final_snps_on_file.csv')
+sum_stats_filter_p_values = pd.read_csv('sum_stats_filter_p_values.csv')
+sum_stats_filter_p_values_5_per = pd.read_csv('sum_stats_filter_p_values_0.05.csv')
+
+final_snps_on_file.columns = ['num','rs']
+snps_and_p_vals = sum_stats_filter_p_values.loc[:,['rs','p_wald']]
+sum_stats_filter_p_values_5_per = sum_stats_filter_p_values.loc[:,['rs','p_wald']]
+
+
+intersect = pd.merge(final_snps_on_file,snps_and_p_vals,how='inner',on=['rs'])
+print(intersect)
