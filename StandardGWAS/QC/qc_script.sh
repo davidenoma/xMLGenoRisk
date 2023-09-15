58fe8659c615842884f6822d7d97a8c7911634bc
David Enoma <davidenoma@gmail.com>
2023-09-15 13:38:02 -0600

    Update qc_script.sh

--- StandardGWAS/QC/qc_script.sh
+++ StandardGWAS/QC/qc_script.sh
@@ -16,6 +16,17 @@ cd HOME/{user}/{path/folder containing your files}
 plink --bfile HapMap_3_r3_1 --missing
 
 Rscript --no-save hist_miss.R
+#Script for QC 
+#EUR is the target genotype prefix
+plink \
+    --bfile EUR \
+    --maf 0.01 \
+    --hwe 1e-6 \
+    --geno 0.01 \
+    --mind 0.01 \
+    --write-snplist \
+    --make-just-fam \
+    --out EUR.QC
 
 
 
