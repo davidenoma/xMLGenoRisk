62968b5d58f7359467b6c3c3968128634d67ae28
David Enoma <davidenoma@gmail.com>
2023-12-13 10:35:19 -0700

    update to second_module.py

--- xgboost_fp/second_module.py
+++ xgboost_fp/second_module.py
@@ -252,10 +252,9 @@ def main(X,Y):
 
         # Important: same train and test split as xgboost optimization codes  by fixing random seed
         for train, test in cv.split(x, y):
-            X_train = x.iloc[train,:]
+            X_train = x[train]
             Y_train = y[train]
-
-            X_test = x.iloc[test,:]
+            X_test = x[test]
             Y_test = y[test]
 
             print(X_train.shape,X_test.shape,Y_train.shape,Y_test.shape)
